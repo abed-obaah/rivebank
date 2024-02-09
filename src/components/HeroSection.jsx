d83d9039465a0be4e7ce46fe225208b8769430f9
Abed chinedu <ubahobaah@gmail.com>
2024-02-09 15:48:57 +0100

    funding

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -34,10 +34,6 @@ export default function HeroSection() {
               <div className="hidden sm:mb-8 sm:flex sm:justify-center">
                 <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-400 ring-1 ring-white/10 hover:ring-white/20">
                   Announcing our next round of funding.{' '}
-                  <a href="#" className="font-semibold text-white">
-                    <span className="absolute inset-0" aria-hidden="true" />
-                    Read more <span aria-hidden="true">&rarr;</span>
-                  </a>
                 </div>
               </div>
               <div className="text-center">
